8c9e069ae0aee3bb9817dd5f4d8168f5a4addddf
Rafał Chomczyk <rchomczyk@icloud.com>
2023-08-01 18:39:49 +0200

    Use string type for specifying channel names in message broker

--- src/main/java/moe/rafal/cory/packet/message/MessageBroker.java
+++ src/main/java/moe/rafal/cory/packet/message/MessageBroker.java
@@ -2,7 +2,7 @@ package moe.rafal.cory.packet.message;
 
 public interface MessageBroker {
 
-  void publish(byte[] channelName, byte[] payload);
+  void publish(String channelName, byte[] payload);
 
-  void observe(byte[] channelName, byte[] payload);
+  void observe(String channelName, byte[] payload);
 }
