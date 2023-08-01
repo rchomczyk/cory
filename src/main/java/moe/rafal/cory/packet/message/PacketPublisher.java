089f5c9506a8d9ae8d2a55517270e04f3d5194e8
Rafał Chomczyk <rchomczyk@icloud.com>
2023-08-01 18:20:28 +0200

    Add packet communication
    * Add packet listener
    
    * Add packet publisher
    
    * Use string type for channel name

---
+++ src/main/java/moe/rafal/cory/packet/message/PacketPublisher.java
@@ -0,0 +1,9 @@
+package moe.rafal.cory.packet.message;
+
+import moe.rafal.cory.packet.Packet;
+
+@FunctionalInterface
+public interface PacketPublisher {
+
+  <T extends Packet> void publish(String channelName, T packet);
+}
